7f913af8c69f559c87163fd3ac36a08a5b39bf98
Vinicios Ramos <viniciosramos0123@gmail.com>
2024-07-08 09:42:55 -0300

    Arquivo de conversao de tipos

---
+++ Python/conversao-tipos-variaveis.py
@@ -0,0 +1,16 @@
+preco1 = 10.30
+preco1 = int(preco1)
+
+preco2 = 10
+preco2 = float(preco2)
+
+preco3 = preco1 // preco2
+
+print(type(preco1), type(preco2), type(preco3))
+
+# Converter para string
+preco = "10.50"
+idade = "28"
+
+preco = float(preco)
+idade = int(idade)
---
+++ Python/input-print.py
@@ -0,0 +1,6 @@
+nome = input('Qual o seu nome?: ')
+sobrenome = input('E o seu sobrenome?: ')
+
+print(nome, sobrenome)
+print(nome, sobrenome, end = "...\n")
+print(nome, sobrenome, sep = "#")
