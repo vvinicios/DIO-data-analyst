5038d8c06ae4089b4df799b10a93aed7c95dab04
Vinicios Ramos <viniciosramos0123@gmail.com>
2024-07-29 17:46:54 -0300

    Adicionando ultimas anotações sobre conjuntos

--- Python/conjunto.py
+++ Python/conjunto.py
@@ -1,4 +1,5 @@
 # Eliminar registros duplicados/repetidos dentro de uma lista 
+# Conjunto sempre representados por {}
 print(set([1, 2, 3, 1, 3, 4]))
 
 print(set("abacaxi"))
@@ -24,3 +25,33 @@ conjunto_difference = conjunto_a.difference(conjunto_b) # Apenas valores diferen
 print(f''' \n Todos numeros juntos: {conjunto_uniao} \n Números que aparecem nos dois conjuntos: {conjunto_intersection} \n Números únicos nos 3 conjuntos: {conjunto_difference} \n'''
       )
 
+# Para acessar um conjunto é preciso transformar-lo em lista
+lista_a = list(conjunto_a)
+print(lista_a)
+
+# ADD - adicionar elementos não existentes
+sorteio = {1, 23, 77, 0, 99, 89, 123, 321}
+sorteio.add(23)
+sorteio.add(21)
+
+# Clear - limpar o conjunto
+sorteio.clear()
+
+# Copy - criar uma cópia do conjunto
+sorteio.copy()
+
+# Discart - remover um valor específico, se passar valor que existe ele descarta, caso contrário ele roda
+sorteio.discart(1)
+
+# POP - eliminar o primeiro valor
+sorteio.pop()
+
+# Remove - remove o retira o elemento específico, caso não exita ele retorna um erro
+sorteio.remove(45)
+
+# Len - informar o tamanho do conjunto
+len(sorteio)
+
+# Verificar valor específico
+1 in sorteio # True
+10 in sorteio # False
